6402b349bcba74981eaa4cf8c0a19bb4da3ccee8
ZKHzhaokaihao <48401595+ZKHzhaokaihao@users.noreply.github.com>
2019-03-25 20:02:56 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,44 @@
-
-void delete(int i,list *ptrl)
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct LNode
 {
-    int j;
-    if(i<1||i>ptrl->last+1)
+    int data;
+    int order;
+    struct LNode*next;
+}LNode;
+int main()
+{
+    struct LNode *pa,*pb,*head;
+    head=(struct LNode*)malloc(sizeof(LNode));
+    pa=head;
+    int i,m,n;
+    printf("输入人数n和上限m\n");
+    scanf("%d %d",&n,&m);
+    for(i=1;i<=n;i++)
     {
-        printf("不存在%d个元素\n",i);
+        int k=0;
+        printf("请依次输入每个人的密码\n");
+        scanf("%d",&k);
+        pa->next=(struct LNode*)malloc(sizeof(LNode));
+        pa=pa->next;
+        pa->data=k;
+        pa->order=i;
     }
-    for(j=i;j<=ptrl->last;j++)
+    pa->next=head->next;
+    pa=head->next;
+    free(head);
+    while(pa->next!=pa)
     {
-        ptrl->data[j-1]=ptrl->data[j];
+        for(i=1;i<m;i++)
+        {
+            pb=pa;
+            pa=pa->next;
+        }
+        printf("%d->",pa->order);
+        m=pa->data;
+        pb->next=pa->next;
+        free(pa);
+        pa=pb->next;
     }
-    ptrl->last--;
+    printf("%d\n",pa->order);
 }
